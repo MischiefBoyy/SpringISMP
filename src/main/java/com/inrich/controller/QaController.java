aa8eedb9c317c2a6cb73bf98081db1812349166c
weq <mischiefboyy@163.com>
2018-01-05 16:20:39 +0800

    weq

--- src/main/java/com/inrich/controller/QaController.java
+++ src/main/java/com/inrich/controller/QaController.java
@@ -82,4 +82,10 @@ public class QaController {
 		
 		return result;
 	}
+	
+	@RequestMapping("/getParent")
+	public String getParent() {
+		List<Map<String,Object>> list=levelTwoDAO.selectNotBase();
+		return OutPrintUtil.getJSONString("success", list); 
+	}
 }
