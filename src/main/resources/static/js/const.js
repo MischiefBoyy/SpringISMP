a6f8495a70901f3035ee17b0be46789e376d332e
weq <mischiefboyy@163.com>
2018-01-22 09:37:50 +0800

    weq  gengxin const

--- src/main/resources/static/js/const.js
+++ src/main/resources/static/js/const.js
@@ -1,3 +1,3 @@
-window.PRJ_ROOT = "http://localhost:80/SpringISMP"; //项目名称
+window.PRJ_ROOT = "http://172.16.6.82:80/SpringISMP"; //项目名称
 window.RET_OK = "ok";   //正常返回标志
 window.RET_ERROR = "error";
